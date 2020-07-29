52d7ac7ebe9a724e378e3a409e1a5b19746fb07c
2hardume <43782095+2hardume@users.noreply.github.com>
2020-07-29 20:44:01 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
-﻿<?php
+<?php
 /*
- * ini_set('display_errors', 0);
- * ini_set('display_startup_errors', 0);
- * error_reporting(0);
+ * Настройка для отображения всех ошибок.
+ * ini_set('error_reporting', E_ALL);
+ * ini_set('display_errors', 1);
+ * ini_set('display_startup_errors', 1);
 */
-ini_set('error_reporting', E_ALL);
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
+error_reporting(0);
 require_once 'config.php';
 require_once 'application/bootstrap.php';
